9ae3b902a319e8017f4a122e040491ae128dc56c
evermomenttvn-boop <evermoment.tvn@gmail.com>
2026-08-01 17:58:33 +0900

    add thumbnail

--- src/App.jsx
+++ src/App.jsx
@@ -194,13 +194,57 @@ setScreen("memory");
     alert("Không thể kết nối máy chủ. Vui lòng thử lại.");
   }
 }
+async function createImageThumbnail(file) {
+  return new Promise((resolve) => {
+    const img = new Image();
 
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+
+      const SIZE = 500;
+
+      canvas.width = SIZE;
+      canvas.height = SIZE;
+
+      const ctx = canvas.getContext("2d");
+
+      const scale = Math.max(
+        SIZE / img.width,
+        SIZE / img.height
+      );
+
+      const width = img.width * scale;
+      const height = img.height * scale;
+
+      const x = (SIZE - width) / 2;
+      const y = (SIZE - height) / 2;
+
+      ctx.drawImage(img, x, y, width, height);
+
+      canvas.toBlob(
+        (blob) => {
+          URL.revokeObjectURL(img.src);
+          resolve(blob);
+        },
+        "image/webp",
+        0.82
+      );
+    };
+
+    img.src = URL.createObjectURL(file);
+  });
+}
   async function uploadFiles(event) {
   const selectedFiles = Array.from(event.target.files);
 
   for (const file of selectedFiles) {
-    const safeName = file.name.replace(/[^a-zA-Z0-9._-]/g, "_");
+    if (file.type.startsWith("image/")) {
+  const thumb = await createImageThumbnail(file);
 
+  console.log("Thumbnail:", thumb);
+}
+    const safeName = file.name.replace(/[^a-zA-Z0-9._-]/g, "_");
+  
 const fileName = `${folderName}/${Date.now()}-${safeName}`;
 
     const { error } = await supabase.storage
